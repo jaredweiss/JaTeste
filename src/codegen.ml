e86da1ab89c4f3e4e77f6addcc0fb2229061aa2e
Andrew Grant <amg2215@columbia.edu>
2016-03-24 13:45:21 -0400

    Setting up program flow from lexer all the way to codegen

--- src/codegen.ml
+++ src/codegen.ml
@@ -1,5 +1,11 @@
-
-module L = Lllvm
+module L = Llvm
 module A = Ast
 
 module StringMap = Map.Make(String)
+
+
+let translate program = 
+	(ignore program);
+	let context = L.global_context () in
+	let the_module = L.create_module context "Jateste" in
+	the_module;
--- src/jateste.ml
+++ src/jateste.ml
@@ -9,5 +9,9 @@ let _ =
 	let ast = Parser.program Scanner.token lexbuf in
 	print_string (eval_prog ast);
 	print_string (string_of_program 0 ast);
+	Semant.check ast;
 	(* print_string (Ast.string_of_program ast) *)
+	let m = Codegen.translate ast in
+	Llvm_analysis.assert_valid_module m;
+	print_string (Llvm.string_of_llmodule m)
 
