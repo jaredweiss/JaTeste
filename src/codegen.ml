b1472ccb3833426ba4beac3d5bbc439bce000e97
Andrew Grant <amg2215@columbia.edu>
2016-03-25 19:21:22 -0400

    Added some comments

--- src/codegen.ml
+++ src/codegen.ml
@@ -22,31 +22,36 @@ let translate (globals, functions, structs) =
 		  A.Primitive(A.Int) -> i32_t
     		| _ -> void_t in
 
- let global_vars =
-    let global_var m (t, n) =
-      let init = L.const_int (ltype_of_typ t) 0
-      in StringMap.add n (L.define_global n init the_module) m in
-    List.fold_left global_var StringMap.empty globals in
+(* HashMap of all global variables *)
+ 	let global_vars =
+         let global_var m (t, n) =
+           let init = L.const_int (ltype_of_typ t) 0
+            in StringMap.add n (L.define_global n init the_module) m in
+        List.fold_left global_var StringMap.empty globals in
 
- let printf_t = L.var_arg_function_type i32_t [| L.pointer_type i8_t |] in
-  let printf_func = L.declare_function "printf" printf_t the_module in
+	(* Here we define the built in print function *)
+ 	let printf_t = L.var_arg_function_type i32_t [| L.pointer_type i8_t |] in
+         let printf_func = L.declare_function "printf" printf_t the_module in
 
+	(* Here we iterate through Ast.functions and add all the function names
+	   to a HashMap *)
 	let function_decls =
-    let function_decl m fdecl =
-      let name = fdecl.A.fname
-      and formal_types =
+         let function_decl m fdecl =
+           let name = fdecl.A.fname
+            and formal_types =
         Array.of_list (List.map (fun (t,_) -> ltype_of_typ t) fdecl.A.formals)
       in let ftype = L.function_type (ltype_of_typ fdecl.A.typ) formal_types in
       StringMap.add name (L.define_function name ftype the_module, fdecl) m in
     List.fold_left function_decl StringMap.empty functions in
 
+	(* Method to build body of function *)
 let build_function_body fdecl =
     let (the_function, _) = StringMap.find fdecl.A.fname function_decls in
     let builder = L.builder_at_end context (L.entry_block the_function) in
 
 	let int_format_str = L.build_global_stringptr "%d\n" "fmt" builder in
 
-	(* This is where we push local variables onto the stack *)
+	(* This is where we push local variables onto the stack and add them to a local HashMap*)
 	let local_vars = 
 	 let add_formal m(t, n) p = L.set_value_name n p;
 	  let local = L.build_alloca (ltype_of_typ t) n builder in
@@ -62,6 +67,7 @@ let build_function_body fdecl =
           (Array.to_list (L.params the_function)) in
       List.fold_left add_local formals fdecl.A.vdecls in
 
+	(* Two places to look for a variable 1) local HashMap 2) global HashMap *)
 	let find_var n = try StringMap.find n local_vars
                  with Not_found -> try StringMap.find n global_vars
                  with Not_found -> raise (Failure ("undeclared variable " ^ n))
@@ -92,6 +98,7 @@ let build_function_body fdecl =
 	| _ -> builder
 	in
 	
+	(* Build the body for this function *)
 	let builder = stmt builder (A.Block fdecl.A.body) in
 		
 	add_terminal builder (match fdecl.A.typ with
@@ -99,6 +106,7 @@ let build_function_body fdecl =
       | _ -> L.build_ret (L.const_int i32_t 0) )
 	in
 	
+	(* Here is where we go through each function and build the body of the function *)
 	List.iter build_function_body functions;
 
 
