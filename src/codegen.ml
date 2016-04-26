f2d2eb7557c8784fe520bd861c69e34acb116c67
Andrew Grant <andrewgrant243@gmail.com>
2016-04-25 22:56:41 -0400

    doubles are weird

--- src/codegen.ml
+++ src/codegen.ml
@@ -239,9 +239,9 @@ let printf_func = L.declare_function "printf" printf_t the_module in
 		| A.Geq -> L.build_icmp L.Icmp.Sge
 		| _ -> raise (Exceptions.BugCatch "Binop")		
 		)
-		| A.Primitive(A.Double) -> 
+		| A.Primitive(A.Double) ->
 		(match op with 
-		  A.Add -> L.build_fadd 
+		  A.Add -> ignore(print_string "j");L.build_fadd 
 		| A.Sub -> L.build_fsub
 		| A.Mult -> L.build_fmul
 		| A.Equal -> L.build_fcmp L.Fcmp.Oeq
